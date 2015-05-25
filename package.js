bf284be677a8502f334b061e6bef550e95b60e72
Max Nowack <max@unsou.de>
2015-05-25 22:02:03 +0200

    initial commit

---
+++ package.js
@@ -0,0 +1,23 @@
+Package.describe({
+  name: 'maximal:server-transform',
+  version: '0.1.0',
+  summary: 'Meteor package to transform documents on publish',
+  git: 'https://github.com/maximalmeteor/server-transform',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+  api.use([
+    'coffeescript'
+  ]);
+
+  api.addFiles('server-transform.coffee');
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('coffeescript');
+  api.use('maximal:server-transform');
+  api.addFiles('tests/server-transform.coffee');
+});
